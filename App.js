922fd3ff67334aea3d8b9d08cf1fe865a1b58d86
Yogesh Rana <51941692+YogeshRana07@users.noreply.github.com>
2020-07-13 13:49:38 +0530

    upload

--- App.js
+++ App.js
@@ -1,53 +1,72 @@
-const area=document.querySelector('.location-timezone');
-const icon=document.querySelector('.icon');
-const temprature=document.querySelector('.temprature-degree');
-const description=document.querySelector('.temprature-description');
+//DOM ELEMENTS AND VARIABLES
+const area = document.querySelector(".location-timezone");
+const icon = document.querySelector(".icon");
+const temprature = document.querySelector(".temprature-degree");
+const description = document.querySelector(".temprature-description");
+const btn = document.querySelector(".button");
+let loc = "Yamunanagar";
+const search = document.querySelector(".search__input");
+const unit=document.querySelector('.degree-section span');
+const degree=document.querySelector('.degree-section');
+//API FETCHING
+const handleData = () => {
+  let long;
+  let lat;
+  
 
-const search=document.querySelector('.search__input');
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-window.addEventListener('load',()=>{
-	let long;
-	let lat;
-	let loc;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `http://api.weatherstack.com/current?access_key=0eb4f5aca782c82fbf81e9c0a8140b50&query=${loc}`;
+      fetch(api).then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+          const { temperature } = data.current;
+          if(loc===data.location.name){
+          area.innerText = data.request.query;
+          icon.setAttribute("src", `${data.current.weather_icons[0]}`);
+          description.textContent = data.current.weather_descriptions;
+          temprature.textContent = temperature;
 
-		search.addEventListener('submit',(e) => {
-					e.preventDefault();
-			        loc=search.value;
-				});
+          var celcius=temperature;
+          
+          var farenheit=(temperature*9/5)+32;
+        
+          degree.addEventListener('click',(e) => {
+            e.preventDefault();
+           if(unit.textContent==='C')
+            {
+     
+              unit.textContent='F';
+               temprature.textContent=Math.floor(farenheit);
 
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(position=>{
-			long=position.coords.longitude;
-			lat=position.coords.latitude;
-
-
-			const proxy="https://cors-anywhere.herokuapp.com/";
-			const api=`${proxy}http://api.weatherstack.com/current?access_key=0eb4f5aca782c82fbf81e9c0a8140b50& query=${loc}`;
-			fetch(api)
-			.then(response=>{
-				return response.json();
-			})
-			.then(data=>{
-			
-			
-			console.log(data);
-			area.innerHTML=data.request.query;
-			const {temperature}=data.current;
-			// var icon1=data.current.weather_icons;
-			icon.src= data.current.weather_icons;
-			description.textContent=data.current.weather_descriptions;
-			temprature.textContent=temperature;
-			
-			// setIcons(icon1,document.querySelector(.icon))
-			});
-		});
-	}
-
-	// function setIcons(icon1,iconID){
-	// 	const skycons=new Skycons({color:pink});
-	// 	if(icon1.includes("https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0006_mist.png")
-	// 	const currentIcon=icon.re
-	// skycons.play();
-	// return skycons.set(iconID,Skycons[currentIcon]);
-	// }
+            }
+            else{
+               unit.textContent='C';
+               temprature.textContent=temperature;
+              }
 });
+
+}
+          else{
+          	alert('Enter the Proper Location and First letter in upperccase 😊😊😊');
+          }
+
+        });
+      });
+    });
+  }
+};
+
+
+//EVENT LISTENERS
+window.addEventListener("load", handleData);
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  loc = search.value;
+  handleData();
+});
+
